93a233f328613989c06b0e80d542af022faeb992
Mahdisaadaat <mahdisaadaat@gmail.com>
2022-07-12 09:42:41 +0430

    Create Boost.js

---
+++ src/components/Boost/Boost.js
@@ -0,0 +1,14 @@
+import "./boost.scss";
+
+const Boost = () => {
+  return (
+    <div className="Boost">
+      <div className="boost-content">
+        <h1>Boost Your Links Today</h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Boost;
